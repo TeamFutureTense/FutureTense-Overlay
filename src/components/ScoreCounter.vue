43f6d01e27009f99b4d42e0928c5f96e45ce09ae
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2026-01-11 18:21:53 -0500

    Basic layout for score counter

---
+++ src/components/ScoreCounter.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+</script>
+<template>
+    <div class="score-counter-container">
+        <div id="score-title">Score</div>
+        <div id="score-count">12,341,234,123</div>
+        <div class="small-score-panel">
+            <div id="acc-count">
+                100.00%
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.score-counter-container {
+    position: absolute;
+    top: 25px;
+    right: 10px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 10px;
+    gap: 5px;
+}
+
+.small-score-panel {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 0px 0px 0px 10px;
+    gap: 10px;
+}
+
+#score-title {
+    font-size: 18px;
+    font-weight: bold;
+    color: #9E9E9E;
+}
+
+#score-count {
+    font-size: 48px;
+    font-weight: bold;
+    color: #ffffff;
+}
+
+#acc-count {
+    font-size: 18px;
+    font-weight: bold;
+    color: #9E9E9E;
+}
+</style>
