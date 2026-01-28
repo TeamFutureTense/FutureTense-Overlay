2bdd72508d58f72850bff616ad77140e2a19a957
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2026-01-28 16:53:43 -0500

    Add basic elements for KeyCounter

---
+++ src/components/KeyCounter.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+<template>
+    <div class="key-counters-container">
+
+    </div>
+</template>
+<style lang="css" scoped>
+.key-counters-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0px;
+    margin: 0px;
+}
+</style>
