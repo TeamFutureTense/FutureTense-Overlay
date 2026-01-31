b222bec0a8a61a6783f4d693a19af8ee8465984d
Bohan Jin <ilovemcmrcreeper@gmail.com>
2026-01-31 10:52:43 -0500

    Add key overlay (#21)
    * Add basic elements for KeyCounter
    
    * Add basic designs for KeyCounter
    
    * Add interactive to components
    
    * Change background color
    
    * Add animations for show/hide

---
+++ src/components/KeyCounter.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { useTosuPreciseStore } from '@/stores/tosuPrecise';
+import { useTosuStore } from '@/stores/tosu';
+import KeyCounterElement from './KeyCounterElement.vue';
+import { computed, onMounted } from 'vue';
+import { animate } from 'animejs';
+
+const tosu = useTosuStore()
+
+const showKeyCounter = computed(() => {
+    return tosu.isInGame === true;
+})
+
+function hideKeyCounterAnimation() {
+    animate(".key-counters-container", {
+        opacity: [1, 0],
+        duration: 400,
+        easing: "easeOutQuart",
+    })
+}
+
+function showKeyCounterAnimation() {
+    animate(".key-counters-container", {
+        opacity: [0, 1],
+        duration: 400,
+        easing: "easeOutQuart",
+    })
+}
+
+watch(showKeyCounter, (newVal, oldVal) => {
+    // show counter
+    if (oldVal === false && newVal === true) {
+        showKeyCounterAnimation()
+    }
+    
+    // hide counter
+    else if (oldVal === true && newVal === false) {
+        hideKeyCounterAnimation()
+    }
+})
+
+onMounted(() => {
+    if (showKeyCounter.value === false) {
+        hideKeyCounterAnimation()
+    }
+})
+
+const tosuPrecise = useTosuPreciseStore()
+
+</script>
+<template>
+    <div class="key-counters-container">
+        <KeyCounterElement :isPressed="tosuPrecise.keys.k1.isPressed" :count="tosuPrecise.keys.k1.count"/>
+        <KeyCounterElement :isPressed="tosuPrecise.keys.k2.isPressed" :count="tosuPrecise.keys.k2.count"/>
+        <KeyCounterElement :isPressed="tosuPrecise.keys.m1.isPressed" :count="tosuPrecise.keys.m1.count"/>
+        <KeyCounterElement :isPressed="tosuPrecise.keys.m2.isPressed" :count="tosuPrecise.keys.m2.count"/>
+    </div>
+</template>
+<style lang="css" scoped>
+.key-counters-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0px;
+    margin: 0px;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+}
+</style>
---
+++ src/components/KeyCounterElement.vue
@@ -0,0 +1,83 @@
+<script setup>
+import { computed } from 'vue';
+
+
+const props = defineProps({
+    isPressed: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    count: {
+        type: Number,
+        required: true,
+        default:0
+    }
+})
+
+const edgeColor = computed(() => {
+    if (props.isPressed) {
+        return "rgba(255,255,255,1)"
+    }
+    else {
+        return "rgba(255,255,255,0.7)"
+    }
+})
+
+const edgeHeight = computed(() => {
+    if (props.isPressed) {
+        return "100%"
+    }
+    else {
+        return "75%"
+    }
+})
+
+const backgroundColor = computed(() => {
+    if (props.isPressed) {
+        return "rgba(255,255,255,0.2)"
+    }
+    else {
+        return "rgba(255,255,255,0)"
+    }
+})
+</script>
+<template>
+    <div class="key-counter-element">
+        <div class="occupier"></div>
+        <div class="key-count">
+            {{ props.count }}
+        </div>
+        <div class="edge"></div>
+    </div>
+</template>
+<style lang="css" scoped>
+.key-counter-element {
+    width: 75px;
+    height: 75px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px;
+    margin: 0px;
+    background-color: v-bind("backgroundColor");
+    transition: all 0.1s;
+}
+.occupier {
+    width: 1px;
+    height: 1px;
+    background-color: #00000000;
+}
+.key-count {
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+}
+.edge {
+    width: 7px;
+    height: v-bind("edgeHeight");
+    background-color: v-bind("edgeColor");
+    transition: all 0.1s;
+}
+</style>
